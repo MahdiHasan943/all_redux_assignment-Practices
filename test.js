ef86d9315d131ef10115783dd9a561bc13f19ad7
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-05-24 10:18:49 +0600

    added new practice problem

--- test.js
+++ test.js
@@ -42,3 +42,66 @@ for (let i = arr.length - 1; i >= 0; i--){
     
 }
 
+
+// 5  find average of array ;
+// const array = [20, 40, 50, 70];
+const make_average = (array) => {
+    let number = 0;
+    for (let i = 0; i < array.length; i++){
+        number += array[i];
+        // console.log(number);
+
+        
+    }
+    var Find = number / array.length;
+    return Find;
+}
+// console.log(make_average( [20,20,20,20,20,40]));
+
+// 6 write a function that check number is even or odd;
+const odd_event = (number) => {
+   
+    if (number % 2 ===0) { 
+        console.log('This is a event number ');
+    }
+    else {
+        console.log('this is a odd number');
+    }
+    return number
+   
+}
+
+//7  leap yrar 
+const leapYear = (year) => {
+   
+    if (year % 4 ===0) { 
+        console.log('This is a leap year ');
+    }
+    else {
+        console.log('this is not a leap year');
+    }
+    return year
+   
+}
+// console.log(leapYear(2029));
+
+
+// 8 find oddSum  of array ;
+// const array = [20, 40, 50, 70];
+const sumOdd = (array) => {
+       let number =0   
+    let sum = 0;
+    for (let i = 0; i < array.length; i++){
+        const index = i;
+        number = array[i];
+        if (number % 2 === 1) {
+    //   console.log(index,number);
+          sum+=number
+        }
+
+        
+    }
+    return sum;
+    // return sum;  
+}
+// console.log(sumOdd([12,33,33,40,11]));
